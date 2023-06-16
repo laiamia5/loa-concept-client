27c272648eea490be86f0457ebcca01b68d9c92c
Laia Mia Perez <laiperez1020@gmail.com>
2023-06-16 16:41:51 -0300

    asd

--- src/App.js
+++ src/App.js
@@ -16,8 +16,10 @@ import { useSelector } from 'react-redux';
 import Admin from './components/admin/Admin'
 import CompraFinalizada from './components/CompraFinalizada';
 import Faqs from './components/Faqs.jsx'
+const host = process.env.REACT_APP_BACKEND_URL
 
 function App() {
+  console.log(host)
 
   let carritoCompleto = useSelector(state => state.carrito)
   const scrollear = () => {
